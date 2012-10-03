6a3597c2b7775799b6d46f890030d593f45d96cf
Francois Zaninotto <fzaninotto@gmail.com>
2012-10-03 08:47:40 +0200

    Check DNS availability in test

--- test.php
+++ test.php
@@ -4,6 +4,14 @@ require __DIR__ .'/vendor/autoload.php';
 
 $faker = Faker\Factory::create();
 $faker->addProvider(new CompanyNameGenerator\FakerProvider($faker));
-for ($i=0; $i < 20; $i++) {
-    echo $faker->companyName, "\n";
+$i = 0;
+while ($i < 20) {
+    $name = $faker->companyName;
+    if (!checkdnsrr('www.' . $name . '.com', 'ANY') && !checkdnsrr($name . '.com', 'ANY')) {
+        echo "\n", $name;
+        $i++;
+    } else {
+        echo '.';
+    }
 }
+echo "\n";
